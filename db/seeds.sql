6c0c19a87665708112e836f15b5239c014ab3123
Dazacher <dazacher2000@yahoo.com>
2020-05-28 12:58:25 -0700

    Added a server.js file and some artwork. Also added seed and schema files.

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name)
+VALUES ("Cheese Burger");
+
+INSERT INTO burgers (burger_name)
+VALUES ("Bacon Cheese Burger");
+
+INSERT INTO burgers (burger_name)
+VALUES ("Mushroom Swiss Truffle Burger");
