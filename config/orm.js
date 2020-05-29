acbb76b799a4f701f67aa8a1f7d918efff4042f3
Dazacher <dazacher2000@yahoo.com>
2020-05-29 05:24:17 -0700

    Getting ready to deploy to Heroku.

---
+++ config/orm.js
@@ -0,0 +1,33 @@
+// const connection = require("./connection");
+
+// Object for all our SQL statement functions.
+const orm = {
+    all: async (tableInput) => {
+      const queryString = `SELECT * FROM ${tableInput}`;
+  
+      const result = await connection.query(queryString);
+  
+      return result;
+    },
+  
+    create: async (table, cols, vals) => {
+      let queryString = `INSERT INTO ${table} (${cols.toString()}) VALUES (${printQuestionMarks(vals.length)})`;
+  
+      console.log(queryString);
+  
+      const result = await connection.query(queryString, vals);
+  
+      return result;
+    },
+    // An example of objColVals would be {name: panther, sleepy: true}
+    update: async (table, objColVals, condition) => {
+      let queryString = `UPDATE ${table} SET ${objToSql(objColVals)} WHERE ${condition}`;
+  
+      console.log(queryString);
+      const result = await connection.query(queryString);
+  
+      return result;
+    }
+  };
+  
+//   module.exports = orm;
---
+++ controllers/burgers-controllers.js
@@ -0,0 +1,33 @@
+// const express = require("express");
+// const router = express.Router();
+// const burgers = require("../models/burger.js");
+
+// Create all our routes and set up logic within those routes where required.
+router.get("/", async (req, res) => {
+  const data = await burgers.all();
+
+  res.render("index", { burgers: data });
+});
+
+router.post("/api/burgers", async (req, res) => {
+  const data = await burgers.create(["burger_name", "devoured"], [req.body.burger_name, req.body.devoured]);
+
+  res.json({ id: data.insertId });
+});
+
+router.put("/api/burgers/:id", async (req, res) => {
+  let condition = `id = ${req.params.id}`;
+
+  console.log("condition", condition);
+
+  const data = await cat.update({ devoured: req.body.devoured }, condition);
+
+  if (data.changedRows === 0) {
+    res.status(404).end();
+  }
+
+  res.status(200).end();
+});
+
+// Export routes for server.js to use.
+// module.exports = router;
